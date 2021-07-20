d67a0490bfd5360806ed045ffc1984f554b7f531
Julian Lopez <jlopezsa@gmail.com>
2021-07-19 23:00:08 -0500

    Programa hola mundo en JAVA

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+// Ejemplo de salida de mensajes de texto
+// Estructura del primer programa
+// Ejemplo de comentários
+
+class HolaMundo{
+	public static void main(String[] args){	// método principal main()
+		System.out.println("Hola Mundo");	// sintaxis para salida de datos
+	}
+}
