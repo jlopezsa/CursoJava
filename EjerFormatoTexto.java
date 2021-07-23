ccce620a876bbbf3e447307692d462edd3c1b05b
Julian Lopez <jlopezsa@gmail.com>
2021-07-22 22:51:37 -0500

    Ejercicio para mostrar como se da formato a un texto impreso en la consola

---
+++ EjerFormatoTexto.java
@@ -0,0 +1,21 @@
+/** Ejemplo para dar formato a un texto en la consola de comandos
+ *  Programa para imprimir el siguiente mensaje en la consola.
+ *  Usar secuencias de escape.
+ * 
+         Información	        Dato
+        -----------------------------------------
+        Inicio del curso:	16 de julio de 2021
+        Fin del curso:	    27 de agosto de 2021
+        Horario:		    18h00 - 20h00
+ */
+public class EjerFormatoTexto {
+    public static void main(String[] args){
+        System.out.println(" ");
+        System.out.println("Información\t\tDato");
+        System.out.println("-------------------------------------------");
+        System.out.println("Inicio del curso:\t16 de julio de 2021");
+        System.out.println("Fin del curso:\t\t27 de agosto de 2021");
+        System.out.println("Horario:\t\t18h00 - 20h00");
+
+    }
+}
